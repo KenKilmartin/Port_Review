c44c6dabeca9d9b13a976d079f0d501013cd1289
Ken <kenkilmartin@gmail.com>
2018-04-03 01:27:00 +0100

    Fixed security issue with easy admin now noticed stars not working as it should wtf

--- src/Migrations/Version20180402235433.php
+++ src/Migrations/Version20180402235433.php
@@ -8,14 +8,14 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180331213613 extends AbstractMigration
+class Version20180402235433 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE port (id INT AUTO_INCREMENT NOT NULL, port_name VARCHAR(255) NOT NULL, photo VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, ingredients VARCHAR(255) NOT NULL, price_range INT NOT NULL, is_public TINYINT(1) NOT NULL, date DATE NOT NULL, reviewed_by VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE port (id INT AUTO_INCREMENT NOT NULL, port_name VARCHAR(255) NOT NULL, photo VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, ingredients VARCHAR(255) NOT NULL, price_range INT NOT NULL, is_public TINYINT(1) NOT NULL, date DATETIME NOT NULL, reviewed_by VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE review (id INT AUTO_INCREMENT NOT NULL, port_id INT DEFAULT NULL, review VARCHAR(255) NOT NULL, place_of_purchase VARCHAR(255) NOT NULL, price_paid DOUBLE PRECISION NOT NULL, num_of_stars NUMERIC(10, 0) NOT NULL, user VARCHAR(255) DEFAULT NULL, date DATE NOT NULL, is_public TINYINT(1) NOT NULL, votes INT DEFAULT 0, INDEX IDX_794381C676E92A9C (port_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE app_users (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(25) NOT NULL, password VARCHAR(64) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json_array)\', UNIQUE INDEX UNIQ_C2502824F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE review ADD CONSTRAINT FK_794381C676E92A9C FOREIGN KEY (port_id) REFERENCES port (id)');
